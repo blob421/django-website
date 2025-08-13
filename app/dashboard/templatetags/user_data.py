7a2c026658c2f263bb9cf0e96056e1c83b490b43
Gabriel Blanchet-Poitras <gabrielbpoitras@gmail.com>
2025-08-12 23:11:21 -0400

    This week schedules are up, fixed a bug with the display order.

--- app/dashboard/templatetags/user_data.py
+++ app/dashboard/templatetags/user_data.py
@@ -10,7 +10,8 @@ register = template.Library()
 @register.filter
 def get_schedules(user, manage = False):
             
-    weeks = WeekRange.objects.all()[:4]
+    weeks = WeekRange.objects.filter().order_by('-starting_day')[:4][::-1]
+  
     week1=weeks[0]
     week2=weeks[1]
     week3=weeks[2]
@@ -35,20 +36,9 @@ def get_schedules(user, manage = False):
         
         schedules = Schedule.objects.filter(user__team = user.userprofile.team)
         last_month_schedules  = schedules.filter(query).order_by('id')
-        week_array = []
-
-        for week in weeks:
-            start_day = week.starting_day
-            end_day = week.end_day
-    
-            week_range = f'{start_day.day} {starting_month[:3]}- {end_day.day} {
-                ending_month[:3]}' 
-            
-            week_array.append(week_range)
-
-      
+  
         return {
-                'weeks': week_array,
+            
                 'week_objects': weeks,
                 'schedules': last_month_schedules,
                 'start_month': starting_month,
